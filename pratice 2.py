254fcb67cf7bc32a62e14aaa38f55f6d076c10ba
Aon Muhammad <145316686+AonAideveloper@users.noreply.github.com>
2023-12-05 10:32:45 +0500

    Add files via upload

---
+++ pratice 2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 09:43:08 2023
+
+@author: web 96
+"""
+#How to find a Even Odd number in python:
+    
+    
+    
+# number = int(input('Enter a number: '))
+
+# if number % 2 == 0:
+#     print(number, 'is an even number.')
+# else:
+#     print(number, 'is an odd number.')
+
+
+##How to find a power of a number in Python
+
+
+
+# base = int(input('Enter the base number: '))
+# exponent = int(input('Enter the exponent: '))
+
+# product = base**exponent
+
+# print(base, 'raised to the power of', exponent, 'is', product)
+
